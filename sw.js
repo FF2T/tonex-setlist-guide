2fd04d1f6e66ac6ad666b48b0ec1add9d5d5bfd8
Sebastien Chemin <seb@chemin.io>
2026-05-13 12:44:53 +0200

    Deploy Backline Phase 7.15 - SetlistsScreen add-song via SongSearchBar

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 // Bump CACHE à chaque release. Le filtre k !== CACHE dans activate purge
 // les anciens caches automatiquement.
 
-const CACHE = 'backline-v103';
+const CACHE = 'backline-v104';
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
