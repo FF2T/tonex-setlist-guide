69a34141a1f2fbb6ea85a2aa325b7c85b25faa35
Sebastien Chemin <seb@chemin.io>
2026-05-16 09:29:47 +0200

    Update prod with Backline v8.14.75 (debug logs démo)

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 // Bump CACHE à chaque release. Le filtre k !== CACHE dans activate purge
 // les anciens caches automatiquement.
 
-const CACHE = 'backline-v174';
+const CACHE = 'backline-v175';
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
