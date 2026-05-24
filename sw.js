1f933daa7c365bc5b0926f287f8f02407712b0ea
Sebastien Chemin <seb@chemin.io>
2026-05-24 13:49:43 +0200

    Update prod with Backline v8.14.191 (Phase 7.55.7 S2 fixes Cowork)

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 // Bump CACHE à chaque release. Le filtre k !== CACHE dans activate purge
 // les anciens caches automatiquement.
 
-const CACHE = 'backline-v290';
+const CACHE = 'backline-v291';
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
