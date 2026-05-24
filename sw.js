3f5c9e1a748d2f65424a226e064aaf758fba77d2
Sebastien Chemin <seb@chemin.io>
2026-05-24 09:18:50 +0200

    Update prod with Backline v8.14.183 (Phase 7.53.2 tombstones ToneNET)

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 // Bump CACHE à chaque release. Le filtre k !== CACHE dans activate purge
 // les anciens caches automatiquement.
 
-const CACHE = 'backline-v282';
+const CACHE = 'backline-v283';
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
