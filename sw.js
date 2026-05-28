c5eea86a655958f68cc84b95a7e1329b2af165b7
Sebastien Chemin <seb@chemin.io>
2026-05-28 12:30:51 +0200

    Update prod with Backline v8.14.288 (fix cohérence Scoring preset basse + groupe ToneX)

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 // Bump CACHE à chaque release. Le filtre k !== CACHE dans activate purge
 // les anciens caches automatiquement.
 
-const CACHE = 'backline-v387';
+const CACHE = 'backline-v388';
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
